d7b53cd68ebbe30494964bf3f038c39a7623b28b
ewfrees <jfrees@bus.wisc.edu>
2018-01-23 09:42:41 -0600

    Model Selection and Intro Chapters Update

--- ShowHide.js
+++ ShowHide.js
@@ -1,8 +1,24 @@
 <script language="javascript">
-function toggle(id1,id2) {    var ele = document.getElementById(id1);    var text = document.getElementById(id2);    if (ele.style.display == "block") {       ele.style.display = "none";       text.innerHTML = "Show Solution";    } else {       ele.style.display = "block";       text.innerHTML = "Hide Solution";    } }
+function toggle(id1,id2) {
+	var ele = document.getElementById(id1); var text = document.getElementById(id2);
+	if (ele.style.display == "block") {ele.style.display = "none"; text.innerHTML = "Show Solution";}
+		else {ele.style.display = "block"; text.innerHTML = "Hide Solution";}}
 </script>
-<script language="javascript">function togglecode(id1,id2) {
-   var ele = document.getElementById(id1);    var text = document.getElementById(id2);
-   if (ele.style.display == "block") {      ele.style.display = "none";      text.innerHTML = "Show R Code";   } 
-      else {  ele.style.display = "block";   text.innerHTML = "Hide R Code";   }}		
-</script> 
+<script language="javascript">
+function togglecode(id1,id2) {
+   var ele = document.getElementById(id1); var text = document.getElementById(id2);
+   if (ele.style.display == "block") {ele.style.display = "none"; text.innerHTML = "Show R Code";}
+      else {ele.style.display = "block"; text.innerHTML = "Hide R Code";}}
+</script>
+<script language="javascript">
+function toggleEX(id1,id2) {
+   var ele = document.getElementById(id1); var text = document.getElementById(id2);
+   if (ele.style.display == "block") {ele.style.display = "none"; text.innerHTML = "Show Example";}
+      else {ele.style.display = "block"; text.innerHTML = "Hide Example";}}
+</script>
+<script language="javascript">
+function toggleTheory(id1,id2) {
+   var ele = document.getElementById(id1); var text = document.getElementById(id2);
+   if (ele.style.display == "block") {ele.style.display = "none"; text.innerHTML = "Show Theory";}
+      else {ele.style.display = "block"; text.innerHTML = "Hide Theory";}}
+</script>
